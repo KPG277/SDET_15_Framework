d40f749fa7a6156501431025f42cadba04940faf
Pooja <Pooja@LAPTOP-QB7QI0P1>
2021-03-15 12:56:30 +0530

    commit to git repo

---
+++ Selenium_Framework/src/test/java/contacttest.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class contacttest {
+@Test
+public void createcontacttest() {
+	System.out.println("execting create contact");
+}
+@Test
+public void modifycontacttest() {
+	System.out.println("");
+}
+}
